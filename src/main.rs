1e98f7425968766c48e634211733c64c1ffdd4e2
Nathan Whyte <nathanwhyte35@gmail.com>
2026-01-15 17:29:51 -0600

    Move env variable loading to own function

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,7 @@ mod config;
 #[tokio::main]
 async fn main() {
     // read in comma-separated list of bearer tokens from env
-    // let tokens_string = match env::var("BEARER_TOKENS") {
-    //     Ok(tokens) => tokens,
-    //     Err(_) => panic!("No BEARER_TOKENS environment variable set!"),
-    // };
-    //
-    // let tokens_list = tokens_string
-    //     .split(',')
-    //     .map(|s| s.trim().to_string())
-    //     .collect::<Vec<String>>();
+    let _bearer_tokens = load_env();
 
     // read in env and config
     let _config = match config::load() {
@@ -25,4 +17,19 @@ async fn main() {
     // run our app with hyper, listening globally on port 3000
     // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     // axum::serve(listener, app).await.unwrap();
+}
+
+fn load_env() -> Vec<String> {
+    // NOTE: temporary, bacon doesn't support env in `run` jobs
+    let tokens_string = std::env::var("BEARER_TOKENS").unwrap_or("token12345".to_string());
+
+    // let tokens_string = match std::env::var("BEARER_TOKENS") {
+    //     Ok(tokens) => tokens,
+    //     Err(_) => panic!("No BEARER_TOKENS environment variable set!"),
+    // };
+
+    tokens_string
+        .split(',')
+        .map(|s| s.trim().to_string())
+        .collect::<Vec<String>>()
 }
