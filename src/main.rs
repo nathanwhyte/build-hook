aed3c3696e8d5911a216a330dfc7fcd9e078874c
Nathan Whyte <nathanwhyte35@gmail.com>
2026-01-19 09:30:39 -0600

    Add shared app state and sample POST handler

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use axum::{Router, routing::post};
+use std::sync::Arc;
+
+use axum::{
+    Router,
+    extract::{Path, State},
+    routing::{get, post},
+};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod config;
+
+struct AppState {
+    bearer_tokens: Vec<String>,
+    config: config::Config,
+}
 
 #[tokio::main]
 async fn main() {
@@ -18,24 +29,30 @@ async fn main() {
 
     // read in comma-separated list of bearer tokens from env
     tracing::debug!("Loading bearer tokens...");
-    let _bearer_tokens = load_env();
+    let bearer_tokens = load_env();
 
     // read in env and config
     tracing::debug!("Loading config...");
-    let _config = match config::load() {
+    let config = match config::load() {
         Ok(cfg) => cfg,
         Err(e) => panic!("Could not load config: {}", e),
     };
 
+    let app_state = Arc::new(AppState {
+        bearer_tokens,
+        config,
+    });
+
     // build our application with a single route
     let app = Router::new()
-        .route("/", post(|| async { "Hello, World!" }))
-        .layer(
-            ServiceBuilder::new()
-                .layer(TraceLayer::new_for_http())
-        );
+        .route("/", get(|| async { "Hello from `build-hook`!" }))
+        .route("/{project}", post(handler))
+        .with_state(app_state)
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     tracing::info!("Server starting on 0.0.0.0:3000");
+
+    // TODO: use the k8s buildx target for image builds
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -56,4 +73,25 @@ fn load_env() -> Vec<String> {
         .split(',')
         .map(|s| s.trim().to_string())
         .collect::<Vec<String>>()
+}
+
+async fn handler(Path(path): Path<String>, State(state): State<Arc<AppState>>) -> String {
+    tracing::debug!("Using tokens: {:?}", state.bearer_tokens);
+
+    match state.config.get(&path) {
+        Some(project_config) => {
+            tracing::info!(
+                "Received build hook for project `{}`, building...",
+                project_config.name
+            );
+            format!(
+                "Received build hook for project `{}`, building...",
+                project_config.name
+            )
+        }
+        None => {
+            tracing::warn!("No configuration found for project `{}`, skipping...", path);
+            format!("No configuration found for project `{}`, skipping...", path)
+        }
+    }
 }
