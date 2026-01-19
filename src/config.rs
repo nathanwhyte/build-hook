1d017dcf51297bcdbe8f9b2b0864e9d62c29a9d1
Nathan Whyte <nathanwhyte35@gmail.com>
2026-01-19 09:30:12 -0600

    Add URL slug to project config, use it as a config map key

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 use url::Url;
 
 #[derive(Debug, Deserialize)]
-pub struct Config {
+pub struct ConfigFile {
     app: AppConfig,
     projects: Vec<ProjectConfig>,
 }
@@ -15,7 +17,8 @@ pub struct AppConfig {
 
 #[derive(Debug, Deserialize)]
 pub struct ProjectConfig {
-    name: String,
+    pub name: String,
+    pub slug: String,
     code: CodeConfig,
     image: ImageConfig,
     deployments: DeploymentConfig,
@@ -40,6 +43,8 @@ pub struct DeploymentConfig {
     resources: Vec<String>,
 }
 
+pub type Config = HashMap<String, ProjectConfig>;
+
 pub fn load() -> Result<Config, String> {
     // read projects config file
     let file_string = match std::fs::read_to_string("config.toml") {
@@ -47,26 +52,38 @@ pub fn load() -> Result<Config, String> {
         Err(_) => panic!("Could not read config.toml file!"),
     };
 
-    let config: Config = match toml::from_str(&file_string) {
+    let config_file: ConfigFile = match toml::from_str(&file_string) {
         Ok(v) => v,
         Err(e) => panic!("Could not parse config.toml file: {}", e),
     };
 
-    validate(&config)?;
+    validate(&config_file)?;
 
-    log(&config);
+    let mut config: Config = HashMap::new();
+
+    config_file.projects.into_iter().for_each(|project| {
+        config.insert(project.slug.clone(), project);
+    });
+
+    log(&config_file.app, &config);
 
     Ok(config)
 }
 
-fn validate(config: &Config) -> Result<(), String> {
+fn validate(config: &ConfigFile) -> Result<(), String> {
     // app.registry should be a valid HTTPS URL
     validate_url(&config.app.registry, "app.registry")?;
+
+    // app.cache is a boolean, no need to validate
 
     for project in &config.projects {
         // project.name should not be empty
         if project.name.trim().is_empty() {
             return Err("project.name must not be empty!".to_string());
+        }
+
+        if project.slug.trim().is_empty() {
+            return Err("project.slug must not be empty!".to_string());
         }
 
         // project.code.url should be a valid URL
@@ -113,14 +130,14 @@ fn validate_url(url: &str, field: &str) -> Result<(), String> {
     Ok(())
 }
 
-fn log(value: &Config) {
-    tracing::debug!("---");
-    tracing::debug!("Image Registry URL: {}", value.app.registry);
-    tracing::debug!("Cache Builds: {}", value.app.cache);
+fn log(app_config: &AppConfig, config_map: &Config) {
+    tracing::debug!("Builds should be cached: {}", app_config.cache);
 
-    for project in &value.projects {
+    tracing::debug!("Loaded {} project(s):", config_map.len());
+
+    for (slug, project) in config_map {
         tracing::debug!("---");
-        tracing::debug!("Project: {}", project.name);
+        tracing::debug!("Project: {}, {}", project.name, slug);
         tracing::debug!("  Code URL: {}", project.code.url);
         tracing::debug!("  Code Branch: {}", project.code.branch);
         tracing::debug!("  Code is Public: {}", project.code.public);
